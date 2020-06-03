571114cde9fbe85ef89c488f3f58ffa4a4dd6717
Raj Agravat <rajagravat@complitech.net>
2020-06-03 11:28:01 +0530

    net_check_code
    aaaaa

---
+++ App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Button, CheckBox } from "react-native";
+import NetInfo from "@react-native-community/netinfo";
+
+const App=()=>
+{
+  let a
+  //const {isInternetReachable,setisInternetReachable} =useState()
+  useEffect(()=>{
+    const unsubscribe = NetInfo.addEventListener((internet) => {
+      console.log("IS INTERNET REACHABLE", internet.isInternetReachable);
+      a=internet.isInternetReachable
+    });
+    return()=>
+    {
+      unsubscribe(); 
+    }    
+  })
+ 
+  return(
+    <View style={{justifyContent:'center',alignItems:'center', flex:1}}>
+        <Button
+          title="check connection"
+          onPress={()=>{
+            if(a)
+            {
+              alert("net is coonected")
+            }
+            else{
+              alert("disconnected")
+            }
+          }}>
+        </Button>
+    </View>
+  )
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
